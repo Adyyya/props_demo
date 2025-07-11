83fafccabd076ebda8456b448f1aeb04f74e655c
Aditya <auryawanshiaditya52@gmail.com>
2025-07-11 11:52:01 +0530

    saved

---
+++ Props_example/App.jsx
@@ -0,0 +1,16 @@
+
+
+ import Student from "./Student"
+
+
+function App() {
+  // cnonst [count,setCount]= useState(0)
+  let vari = "pune"
+  return (
+    <div>
+      <Student city={vari}/>
+    </div>
+  )
+}
+
+export default App
---
+++ Props_example/Student.jsx
@@ -0,0 +1,9 @@
+function Student(props){
+    return(
+        <div> 
+            <h1>City={props.city}</h1>
+            <h3></h3>
+        </div>
+    )
+}
+export default Student;
